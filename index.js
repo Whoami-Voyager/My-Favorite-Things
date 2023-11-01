609942555c7ca3bf57e9c394f64f1208bd25e984
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2023-11-01 11:31:42 -0600

    trying to takeoff

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 const flyingElement = document.getElementById("Flying");
 
 flyingElement.addEventListener("click", function() {
-    flyingElement.style.transition = "top 5s, left 5s";
-
-    flyingElement.style.top = "0";
-    flyingElement.style.left = "0";
+  flyingElement.classList.add("animated");
 });
