edac554897b8f7d350e8d7df556f0d617257abaa
Jonathan McIntosh <jonmcawesome2003@gmail.com>
2023-11-01 11:53:26 -0600

    got it flying, now need to be cliked

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const flyingElement = document.getElementById("Flying");
 
 flyingElement.addEventListener("click", function() {
-  flyingElement.classList.add("animated");
+  // Check if the animated class is already applied
+  if (!flyingElement.classList.contains("animated")) {
+    // Add the animated class to trigger the animation
+    flyingElement.classList.add("animated");
+
+    // Remove the animated class after the animation is complete
+    setTimeout(() => {
+      flyingElement.classList.remove("animated");
+    }, 4); 
+  }
 });
